b75d5b1b86279a143e488e7af43aaa1535426869
Farhan Basheer <farhanfb6@outlook.com>
2021-02-10 13:22:38 +0500

    Commit All

---
+++ Jhochstedler/Jhochstedler/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Jhochstedler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Nexa Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(188, 174);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(173, 45);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Start Process.";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nexa Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Visio File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nexa Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select SVG Folder: ";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nexa Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(188, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Nexa Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(188, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "...";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 239);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreate);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telemetry Play Card Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Jhochstedler/Jhochstedler/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Visio;
+
+
+namespace Jhochstedler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+                Microsoft.Office.Interop.Visio.Application app = new Microsoft.Office.Interop.Visio.Application();
+
+
+
+
+                Document doc = app.Documents.Add(pathVisio);
+
+                Page page;
+                Page page1 = doc.Pages[1];
+
+
+                string path = pathSvg;
+                string[] files = Directory.GetFiles(path);
+
+
+                string[] fileName = Directory.GetFiles(path).Select(file => System.IO.Path.GetFileName(file)).ToArray();
+
+
+                int f = 1;
+                int n;
+
+                page1.Name = fileName[0].ToString();
+                doc.Pages[1].Import(files[0]);
+                doc.Pages[1].ResizeToFitContents();
+
+                for (n = 2; n <= files.Length; n++)
+                {
+
+
+                    page = doc.Pages.Add();
+
+                    page.Name = fileName[f].ToString();
+
+                    doc.Pages[n].Import(files[f]);
+                    doc.Pages[n].ResizeToFitContents();
+                    
+                    f++;
+                    
+                }
+
+                doc.PrintFitOnPages = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("We have got this error. Please Contact Developer" + ex,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+        string pathVisio;
+        string pathSvg;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog slctfile = new OpenFileDialog();
+            slctfile.Title = "Please Select Your Visio File";
+            if (slctfile.ShowDialog() == DialogResult.OK)
+            {
+                pathVisio = slctfile.FileName;
+                label3.Text = pathVisio;
+                button2.TextAlign = ContentAlignment.TopCenter;
+                button2.Image = Properties.Resources.checked_symbol__1_;
+                
+                button2.ImageAlign = ContentAlignment.MiddleRight;
+             
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog slctfile = new FolderBrowserDialog();
+            slctfile.Description = "Please Select Your SVG File Folder";
+            if (slctfile.ShowDialog() == DialogResult.OK)
+            {
+                pathSvg = slctfile.SelectedPath;
+
+                label4.Text = pathSvg;
+                button1.TextAlign = ContentAlignment.TopCenter;
+                button1.Image = Properties.Resources.checked_symbol__1_;
+                button1.ImageAlign = ContentAlignment.MiddleRight;
+
+            }
+        }
+    }
+}
